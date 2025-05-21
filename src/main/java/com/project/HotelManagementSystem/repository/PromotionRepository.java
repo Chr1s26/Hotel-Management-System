869269bf6a36e2f5b1b0abf5e0d85ae50378ce00
Chris <htetkyawswarlinn44@gmail.com>
2025-05-21 20:26:36 +0700

    CITY, PROMOTION and PROPERTY CRUD

--- src/main/java/com/project/HotelManagementSystem/repository/PromotionRepository.java
+++ src/main/java/com/project/HotelManagementSystem/repository/PromotionRepository.java
@@ -1,9 +1,9 @@
 package com.project.HotelManagementSystem.repository;
 
-import com.project.HotelManagementSystem.entity.Payment;
+import com.project.HotelManagementSystem.entity.Promotion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PromotionRepository extends JpaRepository<Payment,Long> {
+public interface PromotionRepository extends JpaRepository<Promotion,Long> {
 }
